860425fb53f9f6d1d5e1172d4135f4fa5824b20f
Antti Peisa <antti.peisa@gmail.com>
2011-11-17 00:50:37 +0200

    Fixed ff event bug on previews

--- InputfieldCropImage/InputfieldCropImage.js
+++ InputfieldCropImage/InputfieldCropImage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	$("a.crop").live("hover", function() {
-		if( event.type === 'mouseover' ) {
+	$("a.crop").live("hover", function(e) {
+		if( e.type === 'mouseenter') {
 			url = $(this).data('thumburl') + "?timestamp=" + new Date().getTime();
 			$(this).append("<img style='position: absolute; z-index: 999;' src="+url+" />");
 		}
