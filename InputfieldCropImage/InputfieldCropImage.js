9182c3b6aa138908706f4cda043fcb6c2e76a5ed
apeisa <antti.peisa@gmail.com>
2012-01-30 12:15:47 +0200

    Removed debug message and removed modal code.

--- InputfieldCropImage/InputfieldCropImage.js
+++ InputfieldCropImage/InputfieldCropImage.js
@@ -11,28 +11,6 @@ $(document).ready(function() {
 	});
 	
 	$(".InputfieldCropImage .InputfieldFileList").live('AjaxUploadDone', function() {
-		console.log("it should");
 		$("a.InputfieldFileLink", $(this)).fancybox(); 
 	}); 
-	
-	/* Modal disabled, cropping huge images is very difficult with modals
-	$("a.crop").live("click", function() {
-		var url = $(this).attr('href');
-		var windowHeight = $(window).height() - 120; 
-		var windowWidth = $(window).width() - 120; 
-		$iframe = $('<iframe id="pwimage_iframe" width="100%" frameborder="0" src="'+url+'"></iframe>');
-		$iframe.parent().css('position', 'fixed').end().dialog({
-					title: "Crop Image", 
-					height: windowHeight,
-					width: windowWidth,
-					position: [60, 60], 
-					modal: true,
-					overlay: {
-						opacity: 0.7,
-						background: "black"
-					}
-				}).width(windowWidth).height(windowHeight);
-		return false;
-	});
-	*/
 });
