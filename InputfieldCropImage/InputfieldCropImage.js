56566c1c4d23313a74ce4d0dcf0b3a5041ebea72
Fokke <ville.saarivaara@gmail.com>
2014-05-05 22:25:20 +0300

    Change deprecated live method to on()

--- InputfieldCropImage/InputfieldCropImage.js
+++ InputfieldCropImage/InputfieldCropImage.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
 
-	$("a.crop").live("hover", function(e) {
+	var magnificOptions = {
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: true,
+		image: {
+			titleSrc: function(item) {
+				return item.el.find('img').attr('alt');
+			}
+		},
+		callbacks: {
+			open: function() {
+				// for firefox, which launches Magnific after a sort
+				if($(".InputfieldFileJustSorted").size() > 0) this.close();
+			}
+		}
+	};
+
+	$("a.crop").on("hover", function(e) {
 		if( e.type === 'mouseover' || e.type === 'mouseenter') {
 			url = $(this).data('thumburl') + "?timestamp=" + new Date().getTime();
 			$(this).append("<img style='position: absolute; z-index: 999;' src="+url+" />");
@@ -9,8 +26,8 @@ $(document).ready(function() {
 			$(this).find("img").remove();
 		}
 	});
-	
-	$(".InputfieldCropImage .InputfieldFileList").live('AjaxUploadDone', function() {
-		$("a.InputfieldFileLink", $(this)).fancybox(); 
-	}); 
+
+	$(".InputfieldCropImage .InputfieldFileList").on('AjaxUploadDone', function() {
+		$("a.InputfieldFileLink", $(this)).magnificPopup(magnificOptions);
+	});
 });
